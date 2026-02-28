4426d01d913af0123806e2d54ec23e5773af6626
PC <126058657+jerinpc@users.noreply.github.com>
2026-02-28 15:37:08 +0530

    Add files via upload

---
+++ now.py
@@ -0,0 +1,26 @@
+import random as rd
+import time
+
+settime=3
+def question():
+    value=rd.randint(0,9)
+    return value
+
+
+
+fstnumber= question()
+secnumber=question()
+
+print(f'{fstnumber} + {secnumber} ?')
+answer = fstnumber+secnumber
+intial_time=time.time()
+userValue=int(input('your value : '))
+final_time= time.time()-intial_time
+
+if answer==userValue:
+    if settime>=final_time:
+        print(f'oww you are right, the answer is {answer} time taken {final_time:.2f} sec speedy!')
+    else:
+        print(f'You are right!, the answer is {answer} but you took {final_time:.2f} sec')
+else:
+    print(f'Your answer is wrong!, the answer is {answer}')
